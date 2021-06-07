986314ae91fc3c792104f156c355a6323f3eb759
Patrick McCoy <pmccoy@gannett.com>
2021-06-07 08:36:09 -0500

    now handles 'ol' & 'ul' parents, along with 'p'. Handles selection change for each parent tag

--- plugins/excludefromsyndication/plugin.js
+++ plugins/excludefromsyndication/plugin.js
@@ -1,44 +1,50 @@
 (function () {
-    // Custom style.
-    var excludeFromSyndication = new CKEDITOR.style({
-        element: 'p',
-        attributes: {
-            'class': 'exclude-from-syndication'
+    const getParent = function(editor) {
+        const parents = editor.elementPath().elements;
+        for (i = 0; i < parents.length; i++) {
+            const parentName = parents[i].getName().toLowerCase();
+            if (parentName === 'p' || parentName === 'ol' || parentName === 'ul') {
+                return parents[i];
+            }
         }
-    });
+        return false;
+    }
 
     CKEDITOR.plugins.add('ExcludeFromSyndication', {
         icons: 'ExcludeFromSyndication',
         init: function(editor) {
-            editor.attachStyleStateChange(excludeFromSyndication, function(state) {
-                !editor.readOnly && editor.getCommand('ExcludeFromSyndication').setState(state);
-            });
+
+            editor.on('selectionChange', function (event) {
+                const parent = getParent(event.editor);
+                if (parent && parent.hasClass('exclude-from-syndication')) {
+                    !editor.readOnly && editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_ON);
+                } else {
+                    !editor.readOnly && editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_OFF);
+                }
+            })
 
             editor.addCommand('ExcludeFromSyndication', {
                 exec: function (editor) {
-                    const parents = editor.elementPath().elements;
-                    for(i = 0; i < parents.length; i++) {
-                        if (parents[i].getName().toLowerCase() === 'p') {
-                            if (parents[i].hasClass('exclude-from-syndication')) {
-                                parents[i].removeClass('exclude-from-syndication');
-                                editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_OFF);
-                            } else {
-                                parents[i].addClass('exclude-from-syndication');
-                                editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_ON);
-                            }
-                            break;
+                    const parent = getParent(editor);
+                    if (parent) {
+                        if (parent.hasClass('exclude-from-syndication')) {
+                            parent.removeClass('exclude-from-syndication');
+                            !editor.readOnly && editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_OFF);
+                        } else {
+                            parent.addClass('exclude-from-syndication');
+                            !editor.readOnly && editor.getCommand('ExcludeFromSyndication').setState(CKEDITOR.TRISTATE_ON);
                         }
                     }
                 }
             });
 
             editor.ui.addButton('ExcludeFromSyndication', {
-                label: 'Exclude From Syndication (SHIFT+ALT+S)',
+                label: 'Exclude From Syndication (SHIFT+ALT+X)',
                 command: 'ExcludeFromSyndication',
                 toolbar: 'ExcludeFromSyndication'
             });
 
-            editor.setKeystroke(CKEDITOR.SHIFT + CKEDITOR.ALT + 53, 'ExcludeFromSyndication'); // SHIFT+ALT+S
+            editor.setKeystroke(CKEDITOR.SHIFT + CKEDITOR.ALT + 88, 'ExcludeFromSyndication'); // SHIFT+ALT+X
         }
     });
 })();
